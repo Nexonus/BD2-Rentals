7f8a61203e79038a1deb3010e6e77d6e27cd649d
Log3rinioo <piotr.winiarczyk.stud@pw.edu.pl>
2026-05-18 20:24:46 +0200

    Hello world page with basic navbar

--- app/wypozyczalnia/views.py
+++ app/wypozyczalnia/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+    context = {"message": "Hello world!"}
+    return render(request, "wypozyczalnia/index.html", context)
